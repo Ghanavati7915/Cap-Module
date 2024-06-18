daefd30b729350d465a72fec22927974aac5ce19
Ahmad Ghanavati <Ahmad_ghanavati@yahoo.com>
2024-06-18 11:54:33 +0330

    - add Try Catch For IndexedDB functions

--- src/runtime/composables/indexedDB.ts
+++ src/runtime/composables/indexedDB.ts
@@ -11,7 +11,8 @@ let DB: IDBDatabase | null; // Database instance
 
 //#region IndexedDB Initialization
 // Function to initialize and open the IndexedDB
-export const IndexDB = (tableName: string): Promise<IDBDatabase> => {
+export const IndexDB = (tableName: string): Promise<IDBDatabase> | null => {
+  try{
   DB_NAME = CapModule.db_name; // Update database name from CapModule
   return new Promise((resolve, reject) => {
     if (DB) {
@@ -24,7 +25,8 @@ export const IndexDB = (tableName: string): Promise<IDBDatabase> => {
 
     request.onsuccess = (e: any) => {
       DB = e.target.result; // Set DB instance on success
-      resolve(DB);
+      if (DB) resolve(DB);
+      else return null
     };
 
     request.onupgradeneeded = (e) => {
@@ -32,33 +34,49 @@ export const IndexDB = (tableName: string): Promise<IDBDatabase> => {
       db.createObjectStore(tableName); // Create object store for tableName
     };
   });
+  }
+  catch (e) {
+    console.error('cap-module ( IndexDBInsert ) Error : ', e)
+    return null;
+  }
 };
 //#endregion
 
 //#region Insert Data
 // Function to insert data into IndexedDB
 export const IndexDBInsert = async (Table: string, field: string, value: any, ExpireTime: any | null = null) => {
-  let db = await IndexDB(Table); // Initialize DB
-  return new Promise<void>((resolve) => {
-    let trans = db.transaction([Table], 'readwrite'); // Start transaction
-    trans.oncomplete = () => {
-      resolve(); // Resolve on transaction complete
-    };
-    let store = trans.objectStore(Table); // Get object store
-    store.put(value, field); // Put value in store
+  try {
+    let db = await IndexDB(Table); // Initialize DB
+    if (db){
+      return new Promise<void>((resolve) => {
+        let trans = db.transaction([Table], 'readwrite'); // Start transaction
+        trans.oncomplete = () => {
+          resolve(); // Resolve on transaction complete
+        };
+        let store = trans.objectStore(Table); // Get object store
+        store.put(value, field); // Put value in store
 
-    if (ExpireTime) {
-      const expire = new Date().setSeconds(ExpireTime); // Set expiration time
-      store.put(expire, `${field}_expireAt`); // Put expiration time in store
+        if (ExpireTime) {
+          const expire = new Date().setSeconds(ExpireTime); // Set expiration time
+          store.put(expire, `${field}_expireAt`); // Put expiration time in store
+        }
+      });
     }
-  });
+    else return null
+  }
+catch (e) {
+    console.error('cap-module ( IndexDBInsert ) Error : ', e)
+  return null;
+  }
 };
 //#endregion
 
 //#region Remove Data
 // Function to remove data from IndexedDB
 export const IndexDBRemove = async (Table: string, field: string) => {
+  try {
   let db = await IndexDB(Table); // Initialize DB
+    if (db){
   return new Promise<void>((resolve) => {
     let trans = db.transaction([Table], 'readwrite'); // Start transaction
     trans.oncomplete = () => {
@@ -67,13 +85,22 @@ export const IndexDBRemove = async (Table: string, field: string) => {
     let store = trans.objectStore(Table); // Get object store
     store.delete(field); // Delete field from store
   });
+    }
+    else return null
+  }
+  catch (e) {
+    console.error('cap-module ( IndexDBRemove ) Error : ', e)
+    return null;
+  }
 };
 //#endregion
 
 //#region Get Data
 // Function to get data from IndexedDB
 export const IndexDBGet = async (Table: string, field: string) => {
+  try{
   let db = await IndexDB(Table); // Initialize DB
+    if (db){
   return new Promise((resolve) => {
     let trans = db.transaction([Table], 'readonly'); // Start read-only transaction
     let response: any = null;
@@ -92,13 +119,22 @@ export const IndexDBGet = async (Table: string, field: string) => {
       }
     };
   });
+  }
+else return null
+  }
+  catch (e) {
+    console.error('cap-module ( IndexDBGet ) Error : ', e)
+    return null;
+  }
 };
 //#endregion
 
 //#region Clear Data
 // Function to clear data from IndexedDB
 export const IndexDBClear = async (Table: string) => {
+  try{
   let db = await IndexDB(Table); // Initialize DB
+    if (db){
   return new Promise<void>((resolve) => {
     let trans = db.transaction([Table], 'readwrite'); // Start transaction
     trans.oncomplete = () => {
@@ -107,5 +143,12 @@ export const IndexDBClear = async (Table: string) => {
     let store = trans.objectStore(Table); // Get object store
     store.clear(); // Clear all data from store
   });
+    }
+    else return null
+  }
+  catch (e) {
+    console.error('cap-module ( IndexDBClear ) Error : ', e)
+    return null;
+  }
 };
 //#endregion
